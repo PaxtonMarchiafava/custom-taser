1667cfc3f99107fd0e2887a1f09828a40a36bc47
Paxton M <65143728+PaxtonMarchiafava@users.noreply.github.com>
2024-12-15 21:49:50 -0500

    Update TENS_SHOCK.ino to use new hardware

--- Arduino_Code/TENS_SHOCK/TENS_SHOCK.ino
+++ Arduino_Code/TENS_SHOCK/TENS_SHOCK.ino
@@ -1,107 +1,141 @@
 
-const int P1 [] = {A3, 2, 6, 10};
-const int P2 [] = {A2, 3, 7, 11};
-const int N1 [] = {A4, A0, 5, 9};
-const int N2 [] = {A1, 4, 8, 12};
+#include <Wire.h>
+
+#define SerialBaudRate 115200
+ // shift register
+const int OE = 9;
+const int STCP = 10;
+const int SHCP = 11;
+const int dataPin = 8;
+byte shiftData [] = {0, 0}; // bytes to push into shift registers
+
+const byte P1 [] [2] = {{1, B00000010}, {1, B00001000}, {1, B00100000}, {1, B10000000}}; // Register, pin
+const byte P2 [] [2] = {{1, B00000001}, {1, B00000100}, {1, B00010000}, {1, B01000000}};
+const byte N1 [] [2] = {{0, B00000001}, {0, B00000100}, {0, B00010000}, {0, B01000000}};
+const byte N2 [] [2] = {{0, B00000010}, {0, B00001000}, {0, B00100000}, {0, B10000000}};
+
+
 boolean Shock_Player [] = {false, false, false, false}; // used to control who gets shocked
 String command = ""; // data from computer
 
 
 void setup() {
 
-    for (int i = 0; i < 4; i++) { // for all players
-	    pinMode(P1[i], OUTPUT); // init output
-        digitalWrite(P1[i], LOW); // dont shock people premature
-	    pinMode(P2[i], OUTPUT);
-        digitalWrite(P2[i], LOW);
-	    pinMode(N1[i], OUTPUT);
-        digitalWrite(N1[i], LOW);
-	    pinMode(N2[i], OUTPUT);
-        digitalWrite(N2[i], LOW);
-    }
+  Wire.begin(); // join i2c bus (address optional for master)
 
-	Serial.begin(115200);
+  pinMode(STCP, OUTPUT);
+  pinMode(SHCP, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(OE, OUTPUT);
+  digitalWrite(OE, LOW); // enable outputs
+  // digitalWrite(OE, HIGH); // enable outputs
+
+	Serial.begin(SerialBaudRate);
+  // shiftData[0] |= N2[0][1];
+  // shiftData[0] |= N2[3][1];
+  // Serial.println(shiftData[0]);
+  // pushUpdate();
+  reset(); // just make sure
 }
 
 void loop() {
-
-	while (Serial.available()) { // read data through usb port from python code
-		command += (char) Serial.read();
-	}
-
-    delay(1);
-    while (Serial.available()) { // read data through usb port from python code
-		command += (char) Serial.read();
-	}
-
     
+  while (Serial.available()) {
+    command += char (Serial.read());
+    delayMicroseconds(86); // 87 because 115200 baud rate
+  }
+  if (command != "") {
+    
+    
+    
+    /* Figure out who to shock here based on serial data
 
-    if (command != 0) {
-        Serial.print(command);
-
-        if (command == "7") { Shock_Player [1] = true; }
-        
-        /*
-        if (command.indexOf("#") != -1) { // If the player was not the winner, shock
-            Shock_Player [1] = true;
-        }
-        if (command.indexOf("#") == -1) {
-            Shock_Player [0] = true;
-        }
-        if (command.indexOf("#") == -1) {
-            Shock_Player [2] = true;
-        }
-        if (command.indexOf("#") == -1) {
-            Shock_Player [3] = true;
-        }
-        */
-
-        command = "";
-        Shock(40, 2000);
+    if (command == "1") {
+      Shock_Player[0] = true;
     }
+    Serial.println(command);
+    
+    */
+    
+    
+    
+    command = "";
+  }
+
+
+  if (Shock_Player[0] || Shock_Player[1] || Shock_Player[2] || Shock_Player[3]) { // if a player is guilty, shock
+    Shock(0.25, 6000); // low freq to see on multimeter. change to 40
+  }
+  
+
 }
 
-void Shock (double freq, double time) {
-    double cycles = freq * (time / 1000); // find how many times to fluctuate shock
+// do the shocking
+void Shock (double freq, double time) { // hz, ms
+  Serial.println("shocking");
 
-    for (int i = 0; i < cycles; i++) {
-        reset(); // reset so you dont short
-        for (int j = 0; j < 4; j++) { // for all players
-            if(Shock_Player[j] == true) { // if deserves it
-                digitalWrite(N1[j], HIGH); // first half of shock
-                digitalWrite(P2[j], HIGH);
-            }
-        }
-        delay(((1/freq) / 2) * 1000);
+  double cycles = freq * (time / 1000); // find how many times to fluctuate shock
 
-        reset();
-        for (int j = 0; j < 4; j++) { // for all players
-            if(Shock_Player[j] == true) { // if deserves it
-                digitalWrite(N2[j], HIGH); // second half of shock
-                digitalWrite(P1[j], HIGH);
-            }
-        }
-        delay(((1/freq) / 2) * 1000);
-        reset();
+  for (int i = 0; i < cycles; i++) {
+    reset(); // reset so you dont short
+    for (int j = 0; j < 4; j++) { // for all players
+      if(Shock_Player[j] == true) { // if deserves it
 
+        shiftData[N1[j][0]] |= N1[j][1];
+        shiftData[P2[j][0]] |= P2[j][1];
 
+        pushUpdate();
+      
+      }
     }
-    for (int j = 0; j < 4; j++) { // reset all values
-        Shock_Player[j] = false;
-    }
+    delay(((1/freq) / 2) * 1000);
 
     reset();
-    delay(1);
+    for (int j = 0; j < 4; j++) { // for all players
+      if(Shock_Player[j] == true) { // if deserves it
+          
+        shiftData[N2[j][0]] |= N2[j][1];
+        shiftData[P1[j][0]] |= P1[j][1];
+        pushUpdate();
+      
+      }
+    }
+    delay(((1/freq) / 2) * 1000);
+    reset();
+
+
+  }
+
+  for (int j = 0; j < 4; j++) { // reset all values
+    Shock_Player[j] = false;
+  }
+
+  reset();
+  Serial.println("done");
 
 }
 
 void reset () { // turn everything off
-    delay(1);
-    for (int j = 0; j < 4; j++) {
-        digitalWrite(P1[j], LOW);
-        digitalWrite(P2[j], LOW);
-        digitalWrite(N1[j], LOW);
-        digitalWrite(N2[j], LOW);
-    }
-    delay(1); // delay to let update (it does not take anywhere close to 1ms but im too lazy to figure out how long it actually takes)
+
+  shiftData[0] = 0;
+  shiftData[1] = 0;
+
+  pushUpdate();
 }
+
+void pushUpdate () {
+
+  digitalWrite(STCP, LOW);
+  shiftOut(dataPin, SHCP, MSBFIRST, shiftData[1]);
+  shiftOut(dataPin, SHCP, MSBFIRST, shiftData[0]);
+  digitalWrite(STCP, HIGH);
+
+  delayMicroseconds(1); // delay to let update (it does not take anywhere close to 1us but im too lazy to figure out how long it actually takes)
+}
+
+void ChangeShockVoltage (byte device, byte NewVoltage) { // device 10
+  Wire.beginTransmission(device);
+  Wire.write(NewVoltage);
+  Wire.endTransmission();
+}
+
